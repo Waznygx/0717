dc32b67412fdeab8a3f415a6aa8f174b418eade9
Waznygx <135837095+Waznygx@users.noreply.github.com>
2023-07-17 20:34:11 +0800

    Add files via upload

--- test2.c
+++ test2.c
@@ -1,15 +1,31 @@
 #include<stdio.h>
 #include<string.h>
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	/*printf("%d\n", sizeof(arr));
+//	printf("%d\n", sizeof(arr+1));
+//	printf("%d\n", sizeof(&arr));
+//	printf("%d\n", sizeof(&arr+1));*/
+//	printf("%d\n", strlen(arr));
+//	printf("%d\n", strlen(arr + 1));
+//	printf("%d\n", strlen(&arr));
+//	printf("%d\n", strlen(&arr + 1));
+//	return 0;
+//}
+
 int main()
 {
-	char arr[] = "abcdef";
-	/*printf("%d\n", sizeof(arr));
-	printf("%d\n", sizeof(arr+1));
-	printf("%d\n", sizeof(&arr));
-	printf("%d\n", sizeof(&arr+1));*/
-	printf("%d\n", strlen(arr));
-	printf("%d\n", strlen(arr + 1));
-	printf("%d\n", strlen(&arr));
-	printf("%d\n", strlen(&arr + 1));
+	int a[3][2] = { (0,1),(2,3),(4,5) };
+	int* p = a[0];
+	/*for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}*/
+	printf("%d", p[0]);
 	return 0;
 }
